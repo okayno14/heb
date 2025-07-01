ff686b0d6d89279672bd33a2edf8aa1eb2fe22cc
aleksandr alekseev <okayno14@gmail.com>
2025-07-01 09:16:58 +0700

    draft human-format: test proto

--- test/heb_tests.erl
+++ test/heb_tests.erl
@@ -4,11 +4,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-some_test_() ->
+oneline_test_() ->
     [
         {"simple tag", fun simple_tag/0},
         {"simple numeric list", fun simple_numeric_list/0},
         {"simple ref", fun simple_ref/0}
+    ].
+
+human_test_() ->
+    [
+        {"simple numeric list human format", fun simple_numeric_list_human/0},
+        {"deep numeric list human format", fun deep_numeric_list_human/0}
     ].
 
 simple_tag() ->
@@ -32,6 +38,35 @@ simple_ref() ->
         }),
 
     ?assertEqual(<<"<a href=\"images/xxx.jpg\"> My Photo !!! </a>">>, heb:build(HTMLDocumentFun)).
+
+simple_numeric_list_human() ->
+    HTMLDocumentFun =
+        heb:tag(<<"ul">>, [], [
+            heb:tag(<<"li">>, [], [<<"a">>]),
+            heb:tag(<<"li">>, [], [<<"b">>]),
+            heb:tag(<<"li">>, [], [<<"c">>])
+        ], #{type => human, format_opts => #{space_tab => 4}}),
+
+    ?debugFmt("~n~ts~n", [heb:build(HTMLDocumentFun)]),
+    ok.
+
+deep_numeric_list_human() ->
+    HTMLDocumentFun =
+        heb:tag(<<"ul">>, [], [
+            heb:tag(<<"ul">>, [], [
+                heb:tag(<<"li">>, [], [<<"1">>]),
+                heb:tag(<<"li">>, [], [<<"2">>])
+            ]),
+            heb:tag(<<"li">>, [], [<<"b">>]),
+            heb:tag(<<"li">>, [], [<<"c">>])
+        ], #{type => human, format_opts => #{space_tab => 4}}),
+
+    ?debugFmt("~n~ts~n", [heb:build(HTMLDocumentFun)]),
+    ok.
+
+%      ?assertEqual(<<"<ul> <li> a </li> <li> b </li> <li> c </li> </ul>">>, heb:build(HTMLDocumentFun)).
+
+
 
 -endif.
 
